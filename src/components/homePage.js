bca08aa7b6dbd60377e75992441eca3d2a9a9706
David <dhahn123@gmail.com>
2019-11-26 14:02:50 -0800

    create button

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class Home extends React.Component {
+
+    exampleMethod() {
+        console.log('JS is running')
+    }
+
     render() {
         return(
             <div>
                 <h1>Home Page</h1>
                 <p>Hello, Welcome to Server Side RenderTown!</p>
+                <button onClick={() => this.exampleMethod()}>Console log some text</button>
             </div>
         )
     }
