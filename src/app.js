6415427a1453a65e7f1b76dcf7adf39639ee4607
David <dhahn123@gmail.com>
2019-11-25 23:31:28 -0800

    create app.js passing in component

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Home from './components/homePage';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <Switch>
+                <Route path="/example" render={props=> (
+                    <Home {...props} />
+                )}/>
+            </Switch>
+        )
+    }
+}
+
+export default App;
