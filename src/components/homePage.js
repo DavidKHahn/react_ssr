4dc7122ef1fb960ad057f2d85921bf594a5cfce3
David <dhahn123@gmail.com>
2019-11-26 14:19:14 -0800

    add meta title to homepage component

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -6,9 +6,20 @@ class Home extends React.Component {
         console.log('JS is running')
     }
 
+    head(){
+        return(
+            <Helment>
+                <title>
+                    React SSR RenderTown Title provided by React Helmet
+                </title>
+            </Helment>
+        )
+    }
+
     render() {
         return(
             <div>
+                {this.head()}
                 <h1>Home Page</h1>
                 <p>Hello, Welcome to Server Side RenderTown!</p>
                 <button onClick={() => this.exampleMethod()}>Console log some text</button>
