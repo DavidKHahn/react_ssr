c5eb3f8ea1825ebac65a829dcc629b8793d5a4a3
David <dhahn123@gmail.com>
2019-11-25 23:28:14 -0800

    create component

---
+++ src/components/homePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Home Page</h1>
+                <p>Hello, Welcome to Server Side RenderTown!</p>
+            </div>
+        )
+    }
+}
